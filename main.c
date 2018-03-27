2adc1be9527bdac4a5ea5e4d44b566fac5ae0664
Roy ash <roykid123@gmail.com>
2018-03-27 18:04:43 +0300

    First

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct
+{
+	long num_of_digits;
+	char *digits;
+} BigNum;
+
+typedef struct Link
+{
+	BigNum num;
+	struct Link *next;
+} LinkedList;
+
+int main()
+{
+//	printf("Hello, World!\n");
+	LinkedList a;
+	
+	scanf("m%s", a.num.digits);
+	return 0;
+}
