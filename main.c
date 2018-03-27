a81f823ec52df011835dacf59a9525b6ce96e776
Roy ash <roykid123@gmail.com>
2018-03-27 22:04:48 +0300

    zxczc

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ extern char *substraction(char *, char*);
 extern char *multipliaction(char*, char*);
 extern char *division(char*, char*);
 
-typedef struct
+typedef struct bigNum
 {
 	long num_of_digits;
 	char *digits;
@@ -41,9 +41,9 @@ bool stackIsEmpty()
 
 int main()
 {
-	LinkedList *stackHead=(LinkedList *) malloc(sizeof(LinkedList));
-	stackHead->num.digits=(char *) malloc(sizeof(char)*10);
-	stackHead->num.num_of_digits=0;
+	LinkedList *stackHead=NULL;//(LinkedList *) malloc(sizeof(LinkedList));
+//	stackHead->num.digits=(char *) malloc(sizeof(char)*10);
+//	stackHead->num.num_of_digits=0;
 	int ch;
 	size_t size=0;
 	while (EOF!=(ch=fgetc(stdin))/* && ch!='\n'*/)
@@ -51,50 +51,37 @@ int main()
 		stackHead->num.digits[stackHead->num.num_of_digits++]=(char) ch;
 		if (stackHead->num.num_of_digits+1==size)
 			increase(stackHead, &size);
-		if (strcmp(ch, "+")==0)
+		if (strcmp(ch, "+")==0 && !stackIsEmpty())
 		{
-			if (!stackIsEmpty())
-			{
-				addition();
-			}
+//			addition();
+			continue;
 		}
-		else
-			if (strcmp(ch, "-")==0)
-			{
-				if (!stackIsEmpty())
-				{
-					substraction();
-				}
-			}
-			else
-				if (strcmp(ch, "*")==0)
-				{
-					if (!stackIsEmpty())
-					{
-						multipliaction();
-					}
-				}
-				else
-					if (strcmp(ch, "/")==0)
-					{
-						if (!stackIsEmpty())
-						{
-							division();
-						}
-					}
-					else
-						if (strcmp(ch, "p")==0)
-						{
-							//TODO pop from stack and print the latest number that entered
-						}
-						else if (strcmp(ch,"q")==0)
-							{
-								exit(1);
-							}
-							else if (strcmp(ch,"c")==0)
-								{
-									//TODO Clear everything from the stack
-								}
+		if (strcmp(ch, "-")==0 && !stackIsEmpty())
+		{
+//			substraction();
+			continue;
+		}
+		if (strcmp(ch, "*")==0 && !stackIsEmpty())
+		{
+//			multipliaction();
+			continue;
+		}
+		if (strcmp(ch, "/")==0 && !stackIsEmpty())
+		{
+//			division();
+			continue;
+		}
+		if (strcmp(ch, "p")==0)
+		{
+			//TODO pop from stack and print the latest number that entered
+			continue;
+		}
+		if (strcmp(ch,"q")==0)
+			exit(1);
+		if (strcmp(ch,"c")==0)
+		{
+			//TODO Clear everything from the stack
+		}
 	}
 	return 0;
 }
